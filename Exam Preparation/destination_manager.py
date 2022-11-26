e7d152ace6cc24011c7d6e995005cfbb990297ad
plam3nk <114010915+plam3nk@users.noreply.github.com>
2022-11-26 15:31:45 +0200

    destination_mapper

---
+++ Exam Preparation/destination_manager.py
@@ -0,0 +1,18 @@
+import re
+
+input_line = input()
+pattern = r"(\=[A-Z][A-Za-z][A-Za-z]+\=|\/[A-Z][A-Za-z][A-Za-z]+\/)"
+
+result = re.findall(pattern, input_line)
+destinations = []
+points = 0
+for item in result:
+    while "=" in item:
+        item = item.replace("=", "")
+    while "/" in item:
+        item = item.replace("/", "")
+    points += len(item)
+    destinations.append(item)
+print("Destinations:", end=" ")
+print(", ".join(destinations))
+print(f"Travel Points: {points}")
